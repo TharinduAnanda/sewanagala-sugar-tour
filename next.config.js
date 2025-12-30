023e463d14c2d21743cefc0f368cfbef9a904f60
Tharindu49 <atharindu788@gmail.com>
2025-12-30 09:49:33 +0530

    10th

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   // Disable webpack cache to avoid permission issues on Windows
   webpack: (config, { isServer }) => {
     config.cache = false;
-    
-    // Exclude emails directory from being processed
-    config.module.rules.push({
-      test: /src\/emails\/.*/,
-      use: 'ignore-loader',
-    });
-    
     return config;
   },
   images: {
@@ -25,10 +18,6 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   },
-  // Explicitly exclude emails directory from page routing
-  pageExtensions: ['tsx', 'ts', 'jsx', 'js'].map(ext => {
-    return ext;
-  }),
 }
 
 module.exports = nextConfig
